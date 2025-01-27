20aca5e1d707fe2b3d3579f18f29fac34aec3683
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-01-27 16:08:09 +0100

    update form

--- src/app/main-page/components/contactform/contactform.component.ts
+++ src/app/main-page/components/contactform/contactform.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contactform',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './contactform.component.html',
   styleUrl: './contactform.component.scss'
 })
 export class ContactformComponent {
+
+  contactData = {
+    name: '',
+    email: '',
+    message: ''
+  }
+
+  onSubmit() {
+    console.log(this.contactData);
+
+  }
+
 
 }
