13b462f74a3d23e2636fbb2c5062b7e1614d8af1
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-01-28 17:34:19 +0100

    http client and php data

--- src/app/main-page/components/contactform/contactform.component.ts
+++ src/app/main-page/components/contactform/contactform.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-contactform',
@@ -18,6 +19,8 @@ export class ContactformComponent {
   imageUrl: string = 'icon/check_box_blank.svg';
 
 
+  http = inject(HttpClient)
+
 
   contactData = {
     name: '',
@@ -25,14 +28,58 @@ export class ContactformComponent {
     message: ''
   }
 
-  onSubmit(ngForm: NgForm) {
 
-    if (ngForm.valid && ngForm.submitted) {
-      console.log(this.contactData);
+
+
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted && this.checkbox && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
     }
   }
 
 
+
+
+  // onSubmit(ngForm: NgForm) {
+  //   this.checkCheckbox();
+  //   if (ngForm.valid && ngForm.submitted && this.checkbox) {
+  //     console.log(this.contactData);
+  //   }
+  // }
+
+
+  checkCheckbox() {
+    if (this.checkbox == false) {
+      this.imageUrl = 'icon/check_box_blank_red.svg'
+    }
+  }
 
 
   changeImage() {
