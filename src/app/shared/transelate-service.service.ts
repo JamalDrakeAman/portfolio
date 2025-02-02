7d0d7c852c382d59cc176a151222628e4aed46d2
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-02-02 11:06:54 +0100

    legals

---
+++ src/app/shared/transelate-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TranselateServiceService {
+
+  language: 'de' | 'en' = 'de';
+
+
+  constructor() {
+
+  }
+
+
+
+}
