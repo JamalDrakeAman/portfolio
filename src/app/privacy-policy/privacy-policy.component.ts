4f0bad672892c0df73f63ea1c25c229e2e5868f7
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-02-07 12:50:42 +0100

    update privacy a tag

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule, ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
+  constructor(private viewportScroller: ViewportScroller) { }
 
+  scrollToElement(elementId: string): void {
+    this.viewportScroller.scrollToAnchor(elementId);
+  }
 }
