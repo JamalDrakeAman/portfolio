fc8ccf79b3e1c4741efa7e8c1893720589b891a4
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-02-07 12:16:35 +0100

    update sections and scss responssive

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { AboutMeSectionComponent } from './about-me-section/about-me-section.component';
 import { SkillsSectionComponent } from './skills-section/skills-section.component';
 import { PortfolioSectionComponent } from './portfolio-section/portfolio-section.component';
 import { ContactSectionComponent } from './contact-section/contact-section.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-page',
@@ -13,11 +14,18 @@ import { ContactSectionComponent } from './contact-section/contact-section.compo
     AboutMeSectionComponent,
     SkillsSectionComponent,
     PortfolioSectionComponent,
-    ContactSectionComponent
+    ContactSectionComponent,
+    CommonModule
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
 export class MainPageComponent {
+  scrollPosition = 0;
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    this.scrollPosition = window.scrollY;
+  }
 
 }
