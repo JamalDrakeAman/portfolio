f7c86b5107c077398a225c73c6b5c2c5c24cfbdc
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-02-15 20:51:24 +0100

    glow mouse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
@@ -16,7 +16,9 @@ import AOS from 'aos';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit(): void {
     AOS.init({
@@ -24,6 +26,24 @@ export class AppComponent implements OnInit {
       easing: 'ease-in-out',
       // once: true       // Animationen nur einmal abspielen
     });
+  }
+
+
+  ngAfterViewInit(): void {
+    // Sicherstellen, dass der Glow-Effekt dynamisch hinzugefügt wird, falls nötig
+    const glowElement = document.createElement('div');
+    glowElement.classList.add('background-glow');
+    document.body.appendChild(glowElement);
+  }
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const { clientX: x, clientY: y } = event;
+    const glow = document.querySelector('.background-glow') as HTMLElement;
+
+    if (glow) {
+      glow.style.background = `radial-gradient(circle 60px at ${x}px ${y}px, rgba(0, 190, 232, 0.3), transparent 80%)`;
+    }
   }
 
 
