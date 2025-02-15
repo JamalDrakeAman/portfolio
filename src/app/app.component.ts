cd9c5d50d947bfb6e11fc37f721b431d06eb8c48
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-02-15 21:02:32 +0100

    fix glow mouse none for touch divices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,16 +18,20 @@ import AOS from 'aos';
 })
 export class AppComponent implements OnInit, AfterViewInit {
 
+  isTouchDevice: boolean = false;
+
   constructor(private elementRef: ElementRef) { }
 
   ngOnInit(): void {
+    // Überprüfen, ob das Gerät ein Touch-Gerät ist
+    this.isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
     AOS.init({
       duration: 500,  // Dauer der Animationen
       easing: 'ease-in-out',
       // once: true       // Animationen nur einmal abspielen
     });
   }
-
 
   ngAfterViewInit(): void {
     // Sicherstellen, dass der Glow-Effekt dynamisch hinzugefügt wird, falls nötig
@@ -36,9 +40,29 @@ export class AppComponent implements OnInit, AfterViewInit {
     document.body.appendChild(glowElement);
   }
 
+  // Mousemove-Event nur ausführen, wenn es kein Touch-Gerät ist
   @HostListener('mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
+    if (this.isTouchDevice) {
+      return; // Verhindert den Glow-Effekt auf Touch-Geräten
+    }
+
     const { clientX: x, clientY: y } = event;
+    const glow = document.querySelector('.background-glow') as HTMLElement;
+
+    if (glow) {
+      glow.style.background = `radial-gradient(circle 60px at ${x}px ${y}px, rgba(0, 190, 232, 0.3), transparent 80%)`;
+    }
+  }
+
+  // Touchmove-Event für Touch-Geräte
+  @HostListener('touchmove', ['$event'])
+  onTouchMove(event: TouchEvent) {
+    if (!this.isTouchDevice) {
+      return; // Verhindert den Touch-Effekt auf Nicht-Touch-Geräten
+    }
+
+    const { clientX: x, clientY: y } = event.touches[0];
     const glow = document.querySelector('.background-glow') as HTMLElement;
 
     if (glow) {
