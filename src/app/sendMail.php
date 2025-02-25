1173eef0fe1a745b9b719c79717e278997ebe106
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-02-25 12:27:48 +0100

    update email

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'jd.aman.web@gmail.com';  
+            $recipient = 'contact@jamalaman.com';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
